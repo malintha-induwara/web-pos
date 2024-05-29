094e88ea107120d9448476783e042f2990e6be32
Induwara <induwara2k@gmail.com>
2024-05-29 22:10:16 +0530

    home page added

--- controller/Navigation.js
+++ controller/Navigation.js
@@ -1,12 +1,21 @@
+document.getElementById("home-button").addEventListener("click", function () {
+  document.getElementById("order-page").style.display = "none";
+  document.getElementById("customer-page").style.display = "none";
+  document.getElementById("item-page").style.display = "none";
+  document.getElementById("home-page").style.display = "flex";
+});
+
 document
   .getElementById("customer-button")
   .addEventListener("click", function () {
+    document.getElementById("home-page").style.display = "none";
     document.getElementById("order-page").style.display = "none";
     document.getElementById("customer-page").style.display = "flex";
     document.getElementById("item-page").style.display = "none";
   });
 
 document.getElementById("order-button").addEventListener("click", function () {
+  document.getElementById("home-page").style.display = "none";
   document.getElementById("order-page").style.display = "flex";
   document.getElementById("customer-page").style.display = "none";
   document.getElementById("item-page").style.display = "none";
@@ -17,6 +26,7 @@ document.getElementById("order-button").addEventListener("click", function () {
 });
 
 document.getElementById("item-button").addEventListener("click", function () {
+  document.getElementById("home-page").style.display = "none";
   document.getElementById("order-page").style.display = "none";
   document.getElementById("customer-page").style.display = "none";
   document.getElementById("item-page").style.display = "flex";
