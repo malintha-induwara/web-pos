7e54adf516ec459d4ede825227ddb38900a7526d
Induwara <induwara2k@gmail.com>
2024-07-25 22:35:32 +0530

    feat: implement SQL Util

--- backend/src/main/java/lk/ijse/webpos/backend/util/SQLUtil.java
+++ backend/src/main/java/lk/ijse/webpos/backend/util/SQLUtil.java
@@ -1,9 +1,39 @@
 package lk.ijse.webpos.backend.util;
 
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SQLUtil {
+    private static DataSource dataSource;
 
+    static {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:/comp/env/jdbc/pos");
+        } catch (NamingException e) {
+            throw new RuntimeException("Failed to lookup DataSource", e);
+        }
+    }
+
+    public static <T> T execute(String sql, Object... args) throws SQLException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pstm = connection.prepareStatement(sql)) {
+
+            for (int i = 0; i < args.length; i++) {
+                pstm.setObject(i + 1, args[i]);
+            }
+
+            if (sql.trim().toLowerCase().startsWith("select")) {
+                return (T) pstm.executeQuery();
+            } else {
+                return (T) (Boolean) (pstm.executeUpdate() > 0);
+            }
+        }
+    }
 
 
 
