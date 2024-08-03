d3aba2f2d3763d4927e1610fcb39ae0fa9707fcc
Induwara <induwara2k@gmail.com>
2024-08-04 00:04:08 +0530

    fix: add missing depenecies in OrderBO impl

--- backend/src/main/java/lk/ijse/webpos/backend/bo/custom/impl/OrderBOImpl.java
+++ backend/src/main/java/lk/ijse/webpos/backend/bo/custom/impl/OrderBOImpl.java
@@ -1,9 +1,16 @@
 package lk.ijse.webpos.backend.bo.custom.impl;
 
 import lk.ijse.webpos.backend.bo.custom.OrderBO;
+import lk.ijse.webpos.backend.dao.DAOFactory;
+import lk.ijse.webpos.backend.dao.custom.ItemDAO;
+import lk.ijse.webpos.backend.dao.custom.OrderDAO;
+import lk.ijse.webpos.backend.dao.custom.OrderDetailDAO;
 import lk.ijse.webpos.backend.dto.OrderDTO;
 
 public class OrderBOImpl implements OrderBO {
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ITEM);
+    OrderDAO orderDAO = (OrderDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ORDER);
+    OrderDetailDAO orderDetailDAO = (OrderDetailDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ORDER_DETAILS);
     @Override
     public boolean saveOrder(OrderDTO order) {
         return false;
