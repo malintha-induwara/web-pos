1888cd27850aef9a748ea41dcecb081b69405c52
Induwara <induwara2k@gmail.com>
2024-08-04 09:31:27 +0530

    refactor: refactor customer dao

--- backend/src/main/java/lk/ijse/webpos/backend/dao/custom/impl/CustomerDAOImpl.java
+++ backend/src/main/java/lk/ijse/webpos/backend/dao/custom/impl/CustomerDAOImpl.java
@@ -2,28 +2,33 @@ package lk.ijse.webpos.backend.dao.custom.impl;
 
 import lk.ijse.webpos.backend.dao.custom.CustomerDAO;
 import lk.ijse.webpos.backend.entity.Customer;
-import lk.ijse.webpos.backend.entity.Item;
 import lk.ijse.webpos.backend.util.SQLUtil;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class CustomerDAOImpl implements CustomerDAO {
+
+
+    private Connection connection;
+
+
     @Override
     public List<Customer> getAll() throws SQLException {
-        List<Map<Integer, Object>> resultCopy = SQLUtil.execute("SELECT * FROM customer");
+        ResultSet resultSet= SQLUtil.execute(connection,"SELECT * FROM customer");
         ArrayList<Customer> customers = new ArrayList<>();
 
-        for (Map<Integer, Object> row : resultCopy) {
+        while (resultSet.next()){
             customers.add(new Customer(
-                    (String) row.get(1),
-                    (String) row.get(2),
-                    (String) row.get(3),
-                    ((java.sql.Date) row.get(4)).toLocalDate(),
-                    (String) row.get(5),
-                    (String) row.get(6)
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getDate(4).toLocalDate(),
+                    resultSet.getString(5),
+                    resultSet.getString(6)
             ));
         }
 
@@ -32,7 +37,7 @@ public class CustomerDAOImpl implements CustomerDAO {
 
     @Override
     public boolean save(Customer customer) throws SQLException {
-        return SQLUtil.execute("INSERT INTO customer VALUES(?,?,?,?,?,?)",
+        return SQLUtil.execute(connection,"INSERT INTO customer VALUES(?,?,?,?,?,?)",
                 customer.getCustomerId(),
                 customer.getFirstName(),
                 customer.getLastName(),
@@ -43,7 +48,7 @@ public class CustomerDAOImpl implements CustomerDAO {
 
     @Override
     public boolean update(String id, Customer customer) throws SQLException {
-        return SQLUtil.execute("UPDATE customer SET customerId=?, firstName=?,lastName=?,dob=?,address=?,mobile=? WHERE customerId=?",
+        return SQLUtil.execute(connection,"UPDATE customer SET customerId=?, firstName=?,lastName=?,dob=?,address=?,mobile=? WHERE customerId=?",
                 customer.getCustomerId(),
                 customer.getFirstName(),
                 customer.getLastName(),
@@ -55,12 +60,17 @@ public class CustomerDAOImpl implements CustomerDAO {
 
     @Override
     public boolean delete(String id) throws SQLException {
-        return SQLUtil.execute("DELETE FROM customer WHERE customerId=?", id);
+        return SQLUtil.execute(connection,"DELETE FROM customer WHERE customerId=?", id);
     }
 
     @Override
     public Customer search(String id) {
         return null;
+    }
+
+    @Override
+    public void setConnection(Connection connection) {
+        this.connection = connection;
     }
 }
 
