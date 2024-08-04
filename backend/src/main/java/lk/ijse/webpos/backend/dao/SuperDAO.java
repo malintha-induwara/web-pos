a22172ce6bb7d2c3223533b4c999d63bdec6c5ca
Induwara <induwara2k@gmail.com>
2024-08-04 09:31:08 +0530

    refactor: refactor sql util

--- backend/src/main/java/lk/ijse/webpos/backend/dao/SuperDAO.java
+++ backend/src/main/java/lk/ijse/webpos/backend/dao/SuperDAO.java
@@ -1,4 +1,7 @@
 package lk.ijse.webpos.backend.dao;
 
+import java.sql.Connection;
+
 public interface SuperDAO {
+    void setConnection(Connection connection);
 }
--- backend/src/main/java/lk/ijse/webpos/backend/util/SQLUtil.java
+++ backend/src/main/java/lk/ijse/webpos/backend/util/SQLUtil.java
@@ -5,10 +5,6 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class SQLUtil {
     private static final DataSource dataSource;
@@ -22,39 +18,20 @@ public class SQLUtil {
     }
 
 
-    public static <T> T execute(String sql, Object... args) throws SQLException {
-        try(Connection connection = dataSource.getConnection()) {
+    public static <T> T execute(Connection connection,String sql, Object... args) throws SQLException {
             PreparedStatement pstm = connection.prepareStatement(sql);
             for (int i = 0; i < args.length; i++) {
                 pstm.setObject(i + 1, args[i]);
             }
             if (sql.trim().toLowerCase().startsWith("select")) {
-                ResultSet resultSet = pstm.executeQuery();
-                List<Map<Integer, Object>> resultCopy = copyResultSet(resultSet);
-                return (T) resultCopy;
+                return (T) pstm.executeQuery();
             } else {
                 return (T) (Boolean) (pstm.executeUpdate() > 0);
-            }
         }
     }
 
-
-
-    private static List<Map<Integer, Object>> copyResultSet(ResultSet resultSet) throws SQLException {
-        List<Map<Integer, Object>> rows = new ArrayList<>();
-        ResultSetMetaData metaData = resultSet.getMetaData();
-        int columnCount = metaData.getColumnCount();
-
-        while (resultSet.next()) {
-            Map<Integer, Object> row = new HashMap<>();
-            for (int i = 1; i <= columnCount; i++) {
-                row.put(i, resultSet.getObject(i));
-            }
-            rows.add(row);
-        }
-        return rows;
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
     }
-
-
 }
 
